545695797f7a96ccfbcd46e080c9ddc0d17ec057
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-10-16 13:36:15 +0200

    not null

--- eux-nav-rinasak-persistence/src/main/resources/db/migration/V3__dokument.sql
+++ eux-nav-rinasak-persistence/src/main/resources/db/migration/V3__dokument.sql
@@ -2,9 +2,9 @@ CREATE TABLE dokument
 (
     dokument_uuid    uuid primary key,
     dokument_info_id varchar(100),
-    sed_id           uuid,
-    sed_versjon      int,
-    sed_type         varchar(100),
+    sed_id           uuid         not null,
+    sed_versjon      int          not null,
+    sed_type         varchar(100) not null,
     nav_rinasak_uuid uuid references nav_rinasak (nav_rinasak_uuid),
     opprettet_bruker varchar(100),
     opprettet_dato   timestamp
