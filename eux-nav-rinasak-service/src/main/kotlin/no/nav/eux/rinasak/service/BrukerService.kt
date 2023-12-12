e24b39225df4d6876630c659c37691d2ee0fb935
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-12-12 15:20:18 +0100

    logging

--- eux-nav-rinasak-service/src/main/kotlin/no/nav/eux/rinasak/service/BrukerService.kt
+++ eux-nav-rinasak-service/src/main/kotlin/no/nav/eux/rinasak/service/BrukerService.kt
@@ -1,5 +1,6 @@
 package no.nav.eux.rinasak.service
 
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.springframework.stereotype.Service
 import kotlin.jvm.optionals.getOrNull
@@ -8,14 +9,21 @@ import kotlin.jvm.optionals.getOrNull
 class BrukerService(
     val tokenValidationContextHolder: TokenValidationContextHolder
 ) {
+     val log = logger {}
 
     fun bruker() =
         tokenValidationContextHolder
             .tokenValidationContext
             ?.firstValidToken
+            ?.also { log.info { "firstValidToken=$it" } }
             ?.getOrNull()
+            ?.also { log.info {  "getOrNull=$it" } }
             ?.jwtTokenClaims
+            ?.also { log.info { "jwtTokenClaims.size=${it.allClaims.size}" } }
+            ?.also { log.info { "jwtTokenClaims=${it}" } }
+            ?.also { log.info { "jwtTokenClaims.keys=${it.allClaims.map { it.key }}" } }
             ?.get("NAVident")
+            ?.also { log.info { "ident=$it" } }
             ?.toString()
             ?: "ukjent"
 }
